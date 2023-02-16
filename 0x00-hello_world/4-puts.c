0befa2bd671bd9f985f34a40fb4a362becd0f73a
Mitchelle Muiruri <muiruri003michelle@gmail.com>
2023-02-16 11:41:01 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
-/** 
- *main - prints "Programming is like building a multilingual puzzle, followed by a new line."
- * return 0
+#include <stdio.h>
+
+/**
+ * main - Entry point 
+ *
+ * Return: Always 0 (Success)
  */
- 
+
 int main(void)
-
 {
-  
-     puts("\"Programming is like building a multilingual puzzle\n");
-
-     return (0);
-
+    puts ("\"Programming is like building a multilingual puzzle");
+        return (0);
 }
