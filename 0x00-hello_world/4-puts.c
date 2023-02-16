28ef2084708a6e582fe79d36058397adfa38ae07
Mitchelle Muiruri <muiruri003michelle@gmail.com>
2023-02-16 11:56:49 -0800

    4-puts.

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - Entry point 
- *
- * Return: Always 0 (Success)
+ * main - prints "Programming is like building a multilingual puzzle, followed by a new line."
+ * return (0).
  */
 
 int main(void)
