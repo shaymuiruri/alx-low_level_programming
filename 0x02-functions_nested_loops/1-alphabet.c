6645779b8d6f56e69c02f34bf28a0ac3a3ecf9b3
Mitchelle Muiruri <muiruri003michelle@gmail.com>
2023-02-21 11:23:40 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,13 +2,18 @@
 
 
 	/**
-	 * main - check the code.
-	 *
-	 * Return: Always 0.
+	 * print_alphabet - print all alphabet in lowercase
 	 */
-	int main(void)
-	{
-	    print_alphabet_x10();
-	    return (0);
-	}
 
+
+	void print_alphabet(void)
+	{
+		char letter;
+
+
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+
+
+		_putchar('\n');
+	}
