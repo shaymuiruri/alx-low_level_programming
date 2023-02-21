44859017e053e0fa674840a41940230a9a1728b4
Mitchelle Muiruri <muiruri003michelle@gmail.com>
2023-02-21 11:59:19 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,28 +1,22 @@
-#include "main.h"
+#ifndef main_h
+	#define main_h
 
 
-	/**
-	 * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
-	 * followed by a new line
-	 */
-	void print_alphabet_x10(void)
-	{
-		char ch;
-		int i;
+	int _putchar(char);
+	void print_alphabet(void);
+	void print_alphabet_x10(void);
+	int _islower(int c);
+	int _isalpha(int c);
+	int print_sign(int n);
+	int _abs(int);
+	int print_last_digit(int);
+	void jack_bauer(void);
+	void times_table(void);
+	int add(int, int);
+	void print_to_98(int n);
+	void print_times_table(int n);
 
 
-		i = 0;
+	#endif
 
 
-		while (i < 10)
-		{
-			ch = 'a';
-			while (ch <= 'z')
-			{
-				_putchar(ch);
-				ch++;
-			}
-			_putchar('\n');
-			i++;
-		}
-	}
