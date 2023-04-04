359be89200b7e70998e53f852f9f17b0e8c7f967
Mitchelle Muiruri <muiruri003michelle@gmail.com>
2023-04-04 12:50:10 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,11 @@
 #ifndef LISTS_H
 #define LISTS_H
 #include <stdlib.h>
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ */
 typedef struct listint_s
 {
 	int n;
